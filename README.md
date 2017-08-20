44941c3a0da53a66285bde061002bdcf4dd7dd38
Javed Shah <javed.shah@gomedia.io>
2017-08-21 00:17:22 +0100

    Committed a couple of tests to test mock JSON responses loaded from assets directory

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # nearby-instagram-posts
+
 A simple app that attempts to show Instagram posts near a user's current location.
+
+Due to Sandbox nature of Instagram apps, the app will only show posts from users that have been added as Sandbox users.
+
+The application starts with logging into a user's Instagram account. Once the login has been successful, application tries to fetch the access token
+associated for current Instagram session.
+If access token is retreived successfully, control gets transferred to another activity that takes care of displaying nearby Instagrams posts.
+
+List of nearby locations is shown inside a drop down list which is updated periodically whenver user location changes a little.
+Once user selects a location from the drop down list, corrsponding Instagram posts are searched for that selected location.
+
+If there are any posts found, the list view loads the post images along side user name, full name and profile picture.
+
+Currently there is no option to change the default radius to search around user's current location.
+
+The application at present does not have any Settings to let user log-out from the current account.
--- app/src/androidTest/java/io/instag/nearbyposts/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/io/instag/nearbyposts/ExampleInstrumentedTest.java
@@ -1,11 +1,21 @@
 package io.instag.nearbyposts;
 
 import android.content.Context;
+import android.content.res.AssetManager;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.io.InputStream;
+import java.util.List;
+
+import io.instag.nearbyposts.model.AccessTokenResponseData;
+import io.instag.nearbyposts.model.NearbyPostsResponseData;
+import io.instag.nearbyposts.model.SearchLocationResponseData;
+import io.instag.nearbyposts.model.data.Data;
+import io.instag.nearbyposts.util.Util;
 
 import static org.junit.Assert.*;
 
@@ -16,11 +26,101 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
-    @Test
-    public void useAppContext() throws Exception {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("io.instag.nearbyposts", appContext.getPackageName());
+    private Context appContext;
+    AssetManager assetManager;
+
+    com.google.gson.Gson gson;
+
+    public ExampleInstrumentedTest() {
+        setup();
+    }
+
+    private void setup() {
+        appContext = InstrumentationRegistry.getTargetContext();
+        assetManager = appContext.getResources().getAssets();
+
+        com.google.gson.GsonBuilder gsonBuilder = new com.google.gson.GsonBuilder();
+        gson = gsonBuilder.create();
+    }
+
+    // Test mock json corresponding to recent posts near a user's current location
+    @Test
+    public void TestNearbyPostsResponseData() throws Exception {
+        InputStream is = null;
+        String jsonData = null;
+
+        is = assetManager.open("nearby_posts_response.json");
+        if (is != null) {
+            jsonData = new String(com.google.common.io.ByteStreams.toByteArray(is));
+
+            is.close();
+        }
+
+        assertTrue(jsonData != null);
+        assertTrue(jsonData.length() > 0);
+
+        assertTrue(gson != null);
+
+        NearbyPostsResponseData nearbyPostsResponseData = gson.fromJson(jsonData, NearbyPostsResponseData.class);
+        assertTrue(nearbyPostsResponseData != null);
+
+        assertTrue(nearbyPostsResponseData.getData() != null);
+        assertTrue(nearbyPostsResponseData.getData().size() > 0);
+
+        assertTrue(nearbyPostsResponseData.getMeta() != null);
+        assertTrue(nearbyPostsResponseData.getMeta().getCode() == 200);
+    }
+
+    // Test mock json response corresponding to access token request
+    @Test
+    public void TestAccessTokenResponseData() throws Exception {
+        InputStream is = null;
+        String jsonData = null;
+
+        is = assetManager.open("access_token_response.json");
+        if (is != null) {
+            jsonData = new String(com.google.common.io.ByteStreams.toByteArray(is));
+            is.close();
+        }
+
+        assertTrue(jsonData != null);
+        assertTrue(jsonData.length() > 0);
+
+        assertTrue(gson != null);
+
+        AccessTokenResponseData accessTokenResponseData = gson.fromJson(jsonData, AccessTokenResponseData.class);
+        assertTrue(accessTokenResponseData != null);
+
+        assertTrue(accessTokenResponseData.getAccessToken() != null);
+
+        assertEquals(accessTokenResponseData.getAccessToken(), "5895701120.043c8f5.12b0d3d41da243fca52dd5589bb7f414");
+    }
+
+    // Test mock json response corresponding to access token request
+    @Test
+    public void TestSearchLocationResponseData() throws Exception {
+        InputStream is = null;
+        String jsonData = null;
+
+        is = assetManager.open("search_location_response.json");
+        if (is != null) {
+            jsonData = new String(com.google.common.io.ByteStreams.toByteArray(is));
+            is.close();
+        }
+
+        assertTrue(jsonData != null);
+        assertTrue(jsonData.length() > 0);
+
+        assertTrue(gson != null);
+
+        SearchLocationResponseData searchLocationResponseData = gson.fromJson(jsonData, SearchLocationResponseData.class);
+        assertTrue(searchLocationResponseData != null);
+
+        assertTrue(searchLocationResponseData.getLocationData() != null);
+        assertTrue(searchLocationResponseData.getLocationData().size() > 0);
+
+        assertTrue(searchLocationResponseData.getMeta() != null);
+        assertTrue(searchLocationResponseData.getMeta().getCode() == 200);
     }
 }
